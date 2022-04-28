cafab1a5fe34f93b61866570f7140ec6d2aea062
LongVT <longvtfx14761@funix.edu.vn>
2022-04-28 21:19:23 +0700

    update component and app

---
+++ src/components/MainComponent.js
@@ -0,0 +1,35 @@
+import './App.css';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './MenuComponents';
+import { DISHES } from '../shared/dishes';
+import DishDetail from './DishDetailClassComponent'
+import React, {Component} from 'react'
+
+class Main extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+      selectedDish: null
+    };
+  }
+  onDishSelected(dishId) {
+    this.setState({ selectedDish: dishId });
+}
+render() {
+return (
+    <div>
+      <Navbar dark color = 'primary'>
+        <div className  = 'container'>
+          <NavbarBrand href = '/'>Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes = {this.state.dishes}
+        onClick = {(dishId) => this.onDishSelected(dishId)}/>
+      <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+    </div>
+  );
+}
+}
+
+export default Main;
--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
-import DishDetail from './DishDetailComponentTest'
+import DishDetail from './DishDetailClassComponent'
 
 class Menu extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            selectedDish: null
-    }
 }
-onDishSelected(dish) {
-    this.setState({ selectedDish: dish });
-}
+
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key = {dish.id} className = 'col-12 col-md-6'>
-                    <Card onClick = {() => this.onDishSelected(dish)}>
+                    <Card onClick = {() => this.props.onClick(dish.id)}>
                         <CardImg width='100%' src = {dish.image} alt = {dish.name}/>
                             
                         <CardImgOverlay>
@@ -33,9 +28,6 @@ onDishSelected(dish) {
             <div className = 'container'>
                 <div className = 'row'>
                         {menu}
-                </div>
-                <div className = 'row'>
-                    <DishDetail dish = {this.state.selectedDish}/>
                 </div>
             </div>
         );
