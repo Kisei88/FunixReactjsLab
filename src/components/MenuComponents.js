6e73d6da04d18b1a488dd8dac947f0503b3884cd
LongVT <longvtfx14761@funix.edu.vn>
2022-04-28 20:45:42 +0700

    lab04_1

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import DishDetail from './DishDetailComponentTest'
 
 class Menu extends Component {
 
@@ -13,28 +14,10 @@ class Menu extends Component {
 onDishSelected(dish) {
     this.setState({ selectedDish: dish });
 }
-renderDish(dish) {
-    if (dish != null) {
-        return (
-            <Card>
-                <CardImg width='100%' src = {dish.image} alt = {dish.name}/>
-                <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        )
-    }
-    else {
-        return (
-            <div></div>
-        )
-    }
-}
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div key = {dish.id} className = 'col-12 col-md-5 m-1'>
+                <div key = {dish.id} className = 'col-12 col-md-6'>
                     <Card onClick = {() => this.onDishSelected(dish)}>
                         <CardImg width='100%' src = {dish.image} alt = {dish.name}/>
                             
@@ -43,15 +26,16 @@ renderDish(dish) {
                         </CardImgOverlay>
                     </Card>
                 </div>
+                
             );
         });
         return (
-            <div className = 'contaniner'>
+            <div className = 'container'>
                 <div className = 'row'>
                         {menu}
                 </div>
                 <div className = 'row'>
-                    {this.renderDish(this.state.selectedDish)}
+                    <DishDetail dish = {this.state.selectedDish}/>
                 </div>
             </div>
         );
