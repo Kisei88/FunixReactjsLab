ed9b92f7fb9d88dd241985117ee149f37e460a78
LongVT <longvtfx14761@funix.edu.vn>
2022-04-29 02:06:16 +0700

    added footer and update app.css

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,7 @@ import { DISHES } from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
 import React, {Component} from 'react';
 import Header from './HeaderComponent';
-
+import Footer from './FooterComponent';
 class Main extends Component {
 constructor(props) {
     super(props);
@@ -21,9 +21,9 @@ render() {
 return (
     <div>
         <Header />
-        <Menu dishes = {this.state.dishes}
-        onClick = {(dishId) => this.onDishSelected(dishId)}/>
+        <Menu dishes = {this.state.dishes} onClick = {(dishId) => this.onDishSelected(dishId)}/>
         <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer />
     </div>
   );
 }
