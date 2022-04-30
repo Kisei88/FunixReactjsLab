5c69df46443563cc65a0a27d48d0f0f7c6689771
LongVT <longvtfx14761@funix.edu.vn>
2022-05-01 00:47:21 +0700

    Revert "added Homecomponent"
    This reverts commit d94422f6b041eb7ac6263dc90abdaf343b48b125.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import Main from './components/MainComponent';
 import React, {Component} from 'react'
-import { BrowserRouter } from 'react-router-dom'
+
 class App extends Component {
 
   render() {
 return (
-    <BrowserRouter>
-      <div>
-        <Main/>
-      </div>
-    </BrowserRouter>
+    <div>
+      <Main/>
+    </div>
   );
 }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,30 +6,23 @@ import DishDetail from './DishDetailComponent';
 import React, {Component} from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-
 class Main extends Component {
 constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      selectedDish: null
     };
   }
+  onDishSelected(dishId) {
+    this.setState({ selectedDish: dishId });
+}
 render() {
-  const HomePage = () => {
-    return (
-      <Home/>
-    )
-  }
 return (
     <div>
         <Header />
-        <Switch>
-          <Route path = '/home' component = {HomePage}/>
-          <Route exact path = '/menu' component = {() => <Menu dishes = {this.state.dishes}/>} />
-          <Redirect to ='/home' />
-        </Switch>
+        <Menu dishes = {this.state.dishes} onClick = {(dishId) => this.onDishSelected(dishId)}/>
+        <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
     </div>
   );
