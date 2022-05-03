bd4e694499b5e90b2d9cef84ceec019c177e83b7
LongVT <longvtfx14761@funix.edu.vn>
2022-05-03 17:24:57 +0700

    update components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,9 +25,23 @@ constructor(props) {
 render() {
   const HomePage = () => {
     return (
-      <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]} 
+      <Home 
+      dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
+
       promotion = {this.state.promotions.filter((promo) => promo.featured)[0]}
-      leader = {this.state.leaders.filter((leader) => leader.featured)[0]}/>
+
+      leader = {this.state.leaders.filter((leader) => leader.featured)[0]}
+      />
+    )
+  }
+
+  const DishWithId = ({match}) => {
+    return(
+      <DishDetail 
+      dish = {this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+
+      comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+      />
     )
   }
 return (
@@ -36,6 +50,7 @@ return (
         <Switch>
           <Route path = '/home' component = {HomePage}/>
           <Route exact path = '/menu' component = {() => <Menu dishes = {this.state.dishes}/>} />
+          <Route path = '/menu/:dishId' component = {DishWithId} />
           <Route path = '/contactus' component = {Contact} />
           <Redirect to ='/home' />
         </Switch>
