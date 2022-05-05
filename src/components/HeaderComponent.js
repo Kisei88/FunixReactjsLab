71b9ab6a20063ef78ee3890de43915f44c6f01f8
LongVT <longvtfx14761@funix.edu.vn>
2022-05-05 22:21:03 +0700

    change headercomponent to functional component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,28 +3,34 @@ import {Navbar, NavbarBrand, Nav, NavbarToggler, NavItem, Collapse, Jumbotron} f
 import {NavLink, Link} from 'react-router-dom';
 
 
-class Header extends Component {
+// class Header extends Component {
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            isNavOpen: false,
-        };
-        this.toggleNav = this.toggleNav.bind(this);
+//     constructor (props) {
+//         super(props);
+//         this.state = {
+//             isNavOpen: false,
+//         };
+//         this.toggleNav = this.toggleNav.bind(this);
+//     }
+        
+    // toggleNav() {
+    //     this.setState({ isNavOpen: !this.state.isNavOpen });
+    // }
+    // render () {
+function Header() {
+    const [isNavOpen, setIsNavOpen] = useState (false)
+    const toggleNav = () =>{
+        setIsNavOpen(!isNavOpen)
     }
-    toggleNav() {
-        this.setState({ isNavOpen: !this.state.isNavOpen });
-    }
-    render () {
         return (
             <React.Fragment>
                 <Navbar dark expand = 'md'>
                     <div className  = 'container'>
-                        <NavbarToggler onClick = {this.toggleNav} />
+                        <NavbarToggler onClick = {toggleNav} />
                         <NavbarBrand className = 'mr-auto' href = '/'>
                             <img src = 'assets/images/logo.png' height ='30' width = '41' alt = 'Ristorante Con Fusion'/>
                         </NavbarBrand>
-                        <Collapse isOpen = {this.state.isNavOpen} navbar>
+                        <Collapse isOpen = {isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className = 'nav-link' to = '/home'>
@@ -63,7 +69,7 @@ class Header extends Component {
             </React.Fragment>
         );
     }
-}
+// }
     
 
 
